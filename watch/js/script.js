e7c78d7a9de2e748264f7746b112170ad6620a28
capan <61876272+capanp@users.noreply.github.com>
2025-01-21 23:49:42 +0300

    Update script.js

--- watch/js/script.js
+++ watch/js/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const episodesList = document.getElementById("episodes");
   
     // JSON dosyasını al
-    fetch("data/episodes.json")
+    fetch("data/animes.json")
       .then((response) => {
         if (!response.ok) {
           throw new Error("JSON dosyası yüklenemedi");
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // ID'ye göre sıralayıp, en büyük 6 id'ye sahip animeleri al
         const sortedEpisodes = episodes.sort((a, b) => b.id - a.id); // Büyükten küçüğe sıralama
         const latestEpisodes = sortedEpisodes.slice(0, 6); // İlk 6 animeyi al
+        const populerAnimes = sortedEpisodes.slice(0, 8); // İlk 8 animeyi al
 
         const name = document.getElementById("populerHeader");
         const des = document.getElementById("populerDes");
@@ -31,6 +32,53 @@ document.addEventListener("DOMContentLoaded", () => {
         image.src = latestEpisodes[0].image;
 
         console.log(latestEpisodes[0]);
+
+        const populerContainer = document.getElementById('populerContainer');
+        const totalContainer = document.getElementById('totalAnimes');
+
+        populerAnimes.forEach(film => {
+          const card = document.createElement('div');
+          card.classList.add('film-card');
+
+          card.onclick = function() {
+            window.location.href = `episode.html?anime=${film.isim}`;
+          }
+
+          card.innerHTML = `
+              <img src="${film.image}" alt="${film.isim}">
+              <h3>${film.isim}</h3>
+          `;
+
+          populerContainer.appendChild(card);
+        });
+
+        episodes.forEach(film => {
+          const card = document.createElement('div');
+          card.classList.add('film-card');
+
+          card.onclick = function() {
+            window.location.href = `episode.html?anime=${film.isim}`;
+          }
+
+          card.innerHTML = `
+              <img src="${film.image}" alt="${film.isim}">
+              <h3>${film.isim}</h3>
+          `;
+
+          totalContainer.appendChild(card);
+        });
+
+        //const response = await fetch('films.json');  // JSON dosyasını yükle
+        //const data = await response.json();         // JSON'u JavaScript objesine çevir
+        const contentDiv = document.getElementById('content');
+
+        episodes.forEach(item => {
+          const div = document.createElement('div');
+          div.classList.add('item');
+          div.innerHTML = `<strong>${item.isim}</strong><br>${item.image}`;
+          contentDiv.appendChild(div);
+        });
+
 
         const divs = document.querySelectorAll('.populerListFirst');
         let currentIndex = 1;
@@ -54,18 +102,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
   
         // Son eklenen animeleri listele
-        latestEpisodes.forEach((episode) => {
-          const li = document.createElement("li");
-          li.innerHTML = `<a href="episode.html?anime=${episode.isim}">${episode.isim}</a>`;
-          latestEpisodesList.appendChild(li);
-        });
+        //latestEpisodes.forEach((episode) => {
+          //const li = document.createElement("li");
+          //li.innerHTML = `<a href="episode.html?anime=${episode.isim}">${episode.isim}</a>`;
+          //latestEpisodesList.appendChild(li);
+        //});
   
         // Tüm animeleri listele
-        episodes.forEach((episode) => {
-          const li = document.createElement("li");
-          li.innerHTML = `<a href="episode.html?anime=${episode.isim}">${episode.isim}</a>`;
-          episodesList.appendChild(li);
-        });
+        //episodes.forEach((episode) => {
+          //const li = document.createElement("li");
+          //li.innerHTML = `<a href="episode.html?anime=${episode.isim}">${episode.isim}</a>`;
+          //episodesList.appendChild(li);
+        //});
       })
       .catch((error) => console.error("Hata:", error));
   });
